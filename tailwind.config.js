d22e83abc49d5dcb404bd6643db8e59e14f9488d
norbeex34 <63997805+norbeex34@users.noreply.github.com>
2026-02-06 16:43:27 -0300

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",  // <--- ESTA LÍNEA ES LA CLAVE
   ],
   theme: {
     extend: {},
